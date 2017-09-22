1ffd025a74af15a2e0d191a77ee7cb3a10074043
scOnTz <piyapat_sam_@hotmail.com>
2017-09-23 00:41:10 +0700

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { TodoService } from './services/todo.service';
+import { HttpModule }  from '@angular/http'
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes = [
+  {path:"", component:UserComponent},
+  {path:"about", component:AboutusComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,83 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  private name: string;
+  private age: number;
+  private email: string;
+  private isEditable:boolean = true;
+  // dictionary
+  private address: {
+    street: string,
+    city: string,
+    province: string,
+    postcode: string
+  }
+  // array
+  private skills: string[];
+  private todoList:Todo[];
+
+  constructor(private todoService : TodoService ) {
+    
+    
+
+  }
+
+  ngOnInit() {
+    this.name = 'Piyapat Plydaung III'
+    this.age = 23
+    this.email = 'piyapat_sam_@hotmail.com'
+
+    this.address = {
+      street: "27 Sirithon5 ",
+      city: "BangPlat",
+      province: "BKK",
+      postcode: "10070"
+    }
+
+    this.skills = ["Programming", "Pingpong", "Soccer"];
+
+    // Call Service
+
+    this.todoService.getTodoList().subscribe((response)=>{
+        this.todoList = response;
+    })
+  }
+
+  
+  
+
+
+
+  addSkill(skill) {
+    this.skills.unshift(skill)
+    return false;
+  }
+
+  delSkill(skill) {
+    this.skills.forEach((element, index) => {
+      if (element == skill) {
+        this.skills.splice(index, 1);
+      }
+    });
+  }
+  toggleEdit(){
+    this.isEditable = !this.isEditable;
+  }
+}
+
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
